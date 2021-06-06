c41d32d1463c9e1859a0291b9d8fb9415b616b3c
Samyak Jain <jainsamyak2300@gmail.com>
2021-06-06 11:15:22 +0530

    final Analysis Changes

--- app/src/main/java/com/example/recruitai/FinalAnalysis.java
+++ app/src/main/java/com/example/recruitai/FinalAnalysis.java
@@ -3,10 +3,14 @@ package com.example.recruitai;
 import android.graphics.Color;
 import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.recruitai.Model.UserClass;
 import com.github.mikephil.charting.animation.Easing;
 import com.github.mikephil.charting.charts.PieChart;
 import com.github.mikephil.charting.components.Legend;
@@ -15,7 +19,13 @@ import com.github.mikephil.charting.data.PieDataSet;
 import com.github.mikephil.charting.data.PieEntry;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import org.jetbrains.annotations.NotNull;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -24,14 +34,18 @@ public class FinalAnalysis extends AppCompatActivity {
     FirebaseAuth firebaseAuth;
     FirebaseUser user;
     String uid;
-    DatabaseReference mDatabaseReference;
+    DatabaseReference mUserDatabaseReference;
     DatabaseReference det;
     String JobID;
     String userId;
 
+    UserClass currentUserData;
+
     HashMap<String, Float> emotionMap = new HashMap<>();
 
     PieChart pieChart;
+
+    TextView nameView, phoneNumberView, resumeScoreView, confidenceScoreView, transcriptView;
 
     @RequiresApi(api = Build.VERSION_CODES.N)
     @Override
@@ -39,15 +53,42 @@ public class FinalAnalysis extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_final_analysis);
 
-//        firebaseAuth= FirebaseAuth.getInstance();
-//        user=firebaseAuth.getCurrentUser();
-//        uid=user.getUid();
-//
-//        if (getIntent() != null) {
-//            userId = getIntent().getStringExtra("UserID");
-//        }
-//
-//        mDatabaseReference = FirebaseDatabase.getInstance().getReference().child("Jobs").child(uid).child("Juser").child(userId);
+        nameView = findViewById(R.id.nameView);
+        phoneNumberView = findViewById(R.id.phoneView);
+        resumeScoreView = findViewById(R.id.ResumeScoreView);
+        confidenceScoreView = findViewById(R.id.ConfidenceScoreViewView);
+        transcriptView = findViewById(R.id.transcriptView);
+
+
+        firebaseAuth = FirebaseAuth.getInstance();
+        user = firebaseAuth.getCurrentUser();
+        uid = user.getUid();
+
+        if (getIntent() != null) {
+            userId = getIntent().getStringExtra("UserID");
+        }
+
+        mUserDatabaseReference = FirebaseDatabase.getInstance().getReference().child("Jobs").child(uid).child("Juser").child(userId);
+        mUserDatabaseReference.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull @NotNull DataSnapshot snapshot) {
+                currentUserData = snapshot.getValue(UserClass.class);
+                assert currentUserData != null;
+                nameView.setText(currentUserData.getName());
+                phoneNumberView.setText(currentUserData.getPhone());
+                resumeScoreView.setText(String.valueOf(currentUserData.getResume_score()));
+                confidenceScoreView.setText(currentUserData.getConfidence_score());
+                transcriptView.setText(currentUserData.getAudio_text());
+
+                Log.d("FinalAnalysisEmotion", currentUserData.getEmotions().getAngry());
+            }
+
+            @Override
+            public void onCancelled(@NonNull @NotNull DatabaseError error) {
+
+            }
+        });
+
 
         pieChart = findViewById(R.id.pie_chart);
 
